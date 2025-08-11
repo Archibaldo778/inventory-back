f9e02196eb4d72df9e04277b483bd0e9b6a83096
Ivan Vyskrebentsev <vyrusivan@gmail.com>
2025-08-11 16:15:53 -0400

    init: backend (Express API)

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// тестовый маршрут
+app.get('/api', (req, res) => {
+  res.json({ message: 'API работает 🚀' });
+});
+
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
